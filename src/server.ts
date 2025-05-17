bc1eda8777e4f30d133934789fde1e31721128d0
Toshihiro Yagi <sylc.yagi@gmail.com>
2025-05-18 08:21:14 +0900

    format

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.send('Hello from App Engine!');
+app.get("/api/hello", (req: Request, res: Response) => {
+	res.send("Hello from App Engine!");
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+	console.log(`Server listening on port ${PORT}...`);
 });
